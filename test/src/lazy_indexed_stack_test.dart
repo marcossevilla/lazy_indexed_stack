1ddd334edba09587c7c63a41784a10f224fa065e
Marcos Sevilla <31174242+marcossevilla@users.noreply.github.com>
2022-05-31 18:48:53 -0600

    test: react 100% code coverage (#3)
    * test: add didUpdateWidget case
    
    * chore: refresh gitignore
    
    * test: improve update index validation

--- test/src/lazy_indexed_stack_test.dart
+++ test/src/lazy_indexed_stack_test.dart
@@ -1,11 +1,63 @@
 // ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lazy_indexed_stack/lazy_indexed_stack.dart';
+
+import '../helpers/helpers.dart';
 
 void main() {
   group('LazyIndexedStack', () {
     test('can be instantiated', () {
       expect(LazyIndexedStack(), isNotNull);
     });
+
+    testWidgets('only renders default index', (tester) async {
+      const lazyIndexedStackKey = Key('lazyIndexedStackKey');
+
+      await tester.pumpApp(
+        LazyIndexedStack(
+          key: lazyIndexedStackKey,
+          children: const [
+            Text('page1'),
+            Text('page2'),
+            Text('page3'),
+          ],
+        ),
+      );
+
+      expect(find.byKey(lazyIndexedStackKey), findsOneWidget);
+      expect(find.text('page1'), findsOneWidget);
+      expect(find.text('page2'), findsNothing);
+      expect(find.text('page3'), findsNothing);
+    });
+
+    testWidgets(
+      'changes current index and renders activated children',
+      (tester) async {
+        var index = 0;
+        await tester.pumpApp(
+          StatefulBuilder(
+            builder: (context, setState) {
+              return LazyIndexedStack(
+                index: index,
+                children: [
+                  ElevatedButton(
+                    child: Text('page1'),
+                    onPressed: () => setState(() => index++),
+                  ),
+                  Text('page2'),
+                ],
+              );
+            },
+          ),
+        );
+        await tester.tap(find.byType(ElevatedButton));
+        await tester.pump();
+        expect(find.text('page1'), findsOneWidget);
+        expect(find.text('page2'), findsOneWidget);
+        expect(find.text('page3'), findsNothing);
+      },
+    );
   });
 }
